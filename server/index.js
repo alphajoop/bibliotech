b3454473c608975314d8d44028a6330c3db49ea8
Alpha DIOP <alphakhoss@gmail.com>
2024-07-17 12:42:04 +0000

    ➕ helmet / express-rate-limit / compression

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
+import compression from 'compression';
 import cors from 'cors';
 import express from 'express';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
 import connectDB from './config/db.js';
 import adminRoutes from './routes/adminRoutes.js';
 import bookRoutes from './routes/bookRoutes.js';
@@ -10,9 +13,22 @@ const app = express();
 // Configuration CORS
 app.use(cors({ origin: process.env.ALLOWED_ORIGIN }));
 
+// Sécurisation des en-têtes HTTP
+app.use(helmet());
+
+// Limitation des requêtes
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limite chaque IP à 100 requêtes par fenêtre de 15 minutes
+});
+app.use(limiter);
+
 // Middleware pour le traitement des données JSON et URL encodées
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Middleware de compression
+app.use(compression());
 
 // Connexion à la base de données
 connectDB();
